56d0a54229c87887ee46e08ab85919ed353dab61
JimVsn <juarvasdi@yandex.ru>
2021-12-24 10:10:14 +0300

    fix: первая итерация

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,10 @@
 const popupOpenButton = document.querySelector('.profile__edit-button');
 const popupCloseButton = document.querySelector('.popup__close');
 const popup = document.querySelector('.popup');
-let popupSave = document.querySelector('.popup__save');
+let popupSave = document.querySelector('.popup__saveform');
 let popupProfile = document.querySelector('.popup__profile');
-let popupName = document.querySelector('.popup__name');
-let popupAboute = document.querySelector('.popup__aboute');
+let popupName = document.querySelector('.popup__input_mod-name');
+let popupAboute = document.querySelector('.popup__input_mod-aboute');
 let profileTitle = document.querySelector('.profile__title');
 let profileSubtitle = document.querySelector('.profile__subtitle');
 
@@ -18,20 +18,19 @@ function closePopup () {
   popup.classList.remove('popup_opened');
 }
 
-popupOpenButton.addEventListener('click', openPopup);
-popupCloseButton.addEventListener('click', closePopup);
-popupSave.addEventListener('click', closePopup);
-
 function formSubmitHandler (evt) {
   evt.preventDefault();
   profileTitle.textContent = popupName.value;
   profileSubtitle.textContent = popupAboute.value;
+  closePopup ();
 }
 
+popupOpenButton.addEventListener('click', openPopup);
+popupCloseButton.addEventListener('click', closePopup);
+popupSave.addEventListener('click', closePopup);
 popupSave.addEventListener('click', formSubmitHandler);
 popupSave.addEventListener('submit', formSubmitHandler);
-
-popup.addEventListener( 'keyup', event => {
+popup.addEventListener( 'keyup', event => { /*этот код отвечает за сохранение и закрытие попапа по кнопке Enter, в соответствии с заданием из чеклиста. Из вашего комментария не совсем понятно, почему он не относится к заданию и его нужно удалить*/
   if( event.code === 'Enter' ) profileTitle.textContent = popupName.value;
   if( event.code === 'Enter' ) profileSubtitle.textContent = popupAboute.value;
   if( event.code === 'Enter' ) closePopup ();
