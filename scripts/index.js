09401cf10e985f61346e10604730f4450197d2f4
JimVsn <juarvasdi@yandex.ru>
2021-12-25 08:53:35 +0300

    Fix: вторая итерация

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,12 @@
 const popupOpenButton = document.querySelector('.profile__edit-button');
 const popupCloseButton = document.querySelector('.popup__close');
 const popup = document.querySelector('.popup');
-let popupSave = document.querySelector('.popup__saveform');
 let popupProfile = document.querySelector('.popup__profile');
-let popupName = document.querySelector('.popup__input_mod-name');
-let popupAboute = document.querySelector('.popup__input_mod-aboute');
+let popupName = document.querySelector('.popup__input_mod_name');
+let popupAboute = document.querySelector('.popup__input_mod_aboute');
 let profileTitle = document.querySelector('.profile__title');
 let profileSubtitle = document.querySelector('.profile__subtitle');
+let formElement = document.querySelector('.popup__profile');
 
 function openPopup () {
   popup.classList.add('popup_opened');
@@ -27,14 +27,9 @@ function formSubmitHandler (evt) {
 
 popupOpenButton.addEventListener('click', openPopup);
 popupCloseButton.addEventListener('click', closePopup);
-popupSave.addEventListener('click', closePopup);
-popupSave.addEventListener('click', formSubmitHandler);
-popupSave.addEventListener('submit', formSubmitHandler);
-popup.addEventListener( 'keyup', event => { /*этот код отвечает за сохранение и закрытие попапа по кнопке Enter, в соответствии с заданием из чеклиста. Из вашего комментария не совсем понятно, почему он не относится к заданию и его нужно удалить*/
-  if( event.code === 'Enter' ) profileTitle.textContent = popupName.value;
-  if( event.code === 'Enter' ) profileSubtitle.textContent = popupAboute.value;
-  if( event.code === 'Enter' ) closePopup ();
-});
+formElement.addEventListener('submit', formSubmitHandler); /* Спасибо за развернутый комментарий!*/
+
+
 
 
 
