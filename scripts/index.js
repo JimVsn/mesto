b309eed5f8360feea767a8763d0681a03436685e
JimVsn <juarvasdi@yandex.ru>
2022-01-11 13:07:22 +0300

    feat: модалки и редактирование кардс

--- scripts/index.js
+++ scripts/index.js
@@ -1,45 +1,120 @@
 const popupOpenButton = document.querySelector('.profile__edit-button');
 const popupCloseButton = document.querySelector('.popup__close');
-const popup = document.querySelector('.popup');
-let popupSave = document.querySelector('.popup__save');
+const popup = document.querySelector('.popup_type_edit');
+const popupEditCard = document.querySelector('.popup_type_edit-card');
+const editCardButton = document.querySelector('.profile__add-button');
+const editCardCloseButton = popupEditCard.querySelector('.popup__close');
+const editForm = popup.querySelector('.popup__profile');
+const editCardForm = popupEditCard.querySelector('.popup__profile');
+const inputName = document.querySelector('.popup__input_mod_card-name');
+const inputLink = document.querySelector('.popup__input_mod_card-link');
+const popupTypeImage = document.querySelector('.popup_type_image');
+const popupImage = document.querySelector('.popup__image');
+const imageTitle = document.querySelector('.popup__image-title')
+const imageCloseButton = document.querySelector('.popup__image-close')
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
 let popupProfile = document.querySelector('.popup__profile');
-let popupName = document.querySelector('.popup__name');
-let popupAboute = document.querySelector('.popup__aboute');
+let popupName = document.querySelector('.popup__input_mod_name');
+let popupAboute = document.querySelector('.popup__input_mod_aboute');
 let profileTitle = document.querySelector('.profile__title');
 let profileSubtitle = document.querySelector('.profile__subtitle');
+let formElement = document.querySelector('.popup__profile');
 
+const cardList = document.querySelector('.elements');
+const cardTempate = document.querySelector('.card-template').content;
 
-function openPopup () {
-  popup.classList.add('popup_opened');
-  popupName.value = profileTitle.textContent;
-  popupAboute.value = profileSubtitle.textContent;
+function deleteHandler (evt) {
+  evt.target.closest('.elements__rectangle').remove();
 }
 
-function closePopup () {
-  popup.classList.remove('popup_opened');
+
+function createCard (cardData) {
+  const cardElement = cardTempate.cloneNode(true);
+  const cardImage = cardElement.querySelector('.elements__element');
+  const cardText = cardElement.querySelector('.elements__text');
+  const deleteButton = cardElement.querySelector('.elements__delete-button');
+  const likeButton = cardElement.querySelector('.elements__group');
+
+  cardText.textContent = cardData.name
+  cardImage.src = cardData.link
+
+  cardImage.addEventListener('click', () => {
+    toggleModal(popupTypeImage)
+    popupImage.src = cardData.link
+    imageTitle.textContent = cardData.name
+  })
+  
+  deleteButton.addEventListener('click', deleteHandler);
+  likeButton.addEventListener('click', () => {
+    likeButton.classList.toggle('elements__group_active')
+  });
+  cardList.prepend(cardElement);
 }
 
-popupOpenButton.addEventListener('click', openPopup);
-popupCloseButton.addEventListener('click', closePopup);
-popupSave.addEventListener('click', closePopup);
+initialCards.forEach(createCard);
+
+function toggleModal (modal) {
+  modal.classList.toggle('popup_opened');
+  editCardForm.reset();
+}
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
   profileTitle.textContent = popupName.value;
   profileSubtitle.textContent = popupAboute.value;
+  toggleModal (popup);
 }
 
-popupSave.addEventListener('click', formSubmitHandler);
-popupSave.addEventListener('submit', formSubmitHandler);
+editCardForm.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  const nameValue = inputName.value;
+  const linkValue = inputLink.value;
+  const objct = {
+    name: nameValue,
+    link: linkValue
+  };
+  createCard (objct);
+  toggleModal (popupEditCard);
+})
 
-popup.addEventListener( 'keyup', event => {
-  if( event.code === 'Enter' ) profileTitle.textContent = popupName.value;
-  if( event.code === 'Enter' ) profileSubtitle.textContent = popupAboute.value;
-  if( event.code === 'Enter' ) closePopup ();
+editCardButton.addEventListener('click', () => toggleModal (popupEditCard));
+editCardCloseButton.addEventListener('click', () => toggleModal (popupEditCard));
+
+popupOpenButton.addEventListener('click', () => {
+  toggleModal (popup);
+  popupName.value = profileTitle.textContent;
+  popupAboute.value = profileSubtitle.textContent;
 });
+popupCloseButton.addEventListener('click', () => toggleModal (popup));
 
+formElement.addEventListener('submit', formSubmitHandler);
 
-
-
-
+imageCloseButton.addEventListener('click', () => {
+  toggleModal(popupTypeImage)
+})
 
